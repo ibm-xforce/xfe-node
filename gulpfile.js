e5f931eaf749fea6448425d7b31eb2a7c8c3c3bf
Cameron Will <scwill@us.ibm.com>
2016-02-19 15:15:49 -0500

    [chore] Enable coveralls reporting

--- gulpfile.js
+++ gulpfile.js
@@ -6,6 +6,8 @@ var tslint = require('gulp-tslint');
 var babel = require("gulp-babel");
 var mocha = require('gulp-mocha');
 var typedoc = require("gulp-typedoc");
+var istanbul = require('gulp-istanbul');
+var coveralls = require('gulp-coveralls');
 
 var paths = {
   tscripts: {
@@ -72,6 +74,22 @@ gulp.task('lint:default', function() {
 gulp.task('test', ['test:default']);
 gulp.task('test:default', function() {
   "use strict";
-  return gulp.src(paths.tests, {read: false})
-    .pipe(mocha());
+  return gulp.src("./dist/**/*.js")
+    .pipe(istanbul({ includeUntested: true }))
+    .pipe(istanbul.hookRequire())
+    .on('finish', function() {
+      // Specify server specs
+      gulp.src(paths.tests, {read: false})
+        .pipe(mocha({
+          reporter: 'spec',
+          timeout: 20000
+        }))
+        // Write reports to Istanbul
+        .pipe(istanbul.writeReports())
+    });
+});
+
+gulp.task('coveralls', function () {
+  return gulp.src('./coverage/lcov.info')
+    .pipe(coveralls());
 });
